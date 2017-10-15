0090bb85fcde639dbe2d928c4b73850f8ba841ad
Steve Witman <stevewitman@gmail.com>
2017-10-14 22:23:57 -0600

    Enable basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Page1Component } from './page-1/page-1.component';
+import { Page2Component } from './page-2/page-2.component';
+import { Page3Component } from './page-3/page-3.component';
+
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'page1',
+    component: Page1Component
+  },
+  {
+    path: 'page2',
+    component: Page2Component
+  },
+  {
+    path: 'page3',
+    component: Page3Component
+  },
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page2Component,
+    Page1Component,
+    Page3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
