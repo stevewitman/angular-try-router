9ab685b27759695f5c37e6191b13582570475e8b
Steve Witman <stevewitman@gmail.com>
2017-10-14 23:58:44 -0600

    Get current url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private route:ActivatedRoute) {
+    console.log(route);
+  }
+
+  ngOnInit () {
+  }
+
 }
